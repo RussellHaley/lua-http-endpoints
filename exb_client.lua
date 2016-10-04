ee61fb1291571762fc58ff997ea46d7706b54021
russellh <rhaley@gmail.com>
2016-10-04 00:02:39 -0700

    Changed some patterns. Get Shutdown variable to work and close the application.

--- exb_client.lua
+++ exb_client.lua
@@ -32,13 +32,28 @@ local upd = i.UpdateInstrumentation
 --local conf = configuration.new("/etc/rc.conf",true)
 local conf = configuration.new("exb_client.conf", false, false)
 i.debug_file_path = conf.base_path .. "/" .. conf.debug_file_name
+local debug_file;
 
 --- Shutdown flag. Set to True to end all processes
 local Shutdown = false
 --- Debug flag. Enables Debugging output from the client.
 DEBUG = arg[1] or false
 
+local function StopServices()
+    Shutdown = true
+    print("shutdown", Shutdown)
+end
 
+local function Log(level, fmt, ...)
+    local pkt = os.date("%Y-%m-%d_%H%M%S") .. "-" .. level .. ": " .. string.format(fmt, ...) .. "\n";
+    if level == "error" then
+        pkt = debug.traceback(pkt, 2)
+    end;
+    if DEBUG then
+        print(pkt)
+    end
+    return debug_file:write(pkt)
+end
 
 --- Writes errors to a file.
 -- This needs serious work, or you should
@@ -46,26 +61,19 @@ DEBUG = arg[1] or false
 -- param: errno The error number provided by the exit call
 -- param: err The error message provided by the exit call
 -- param: debugOut true outputs the info to stdio
-local function WriteError(errno, err, debugOut)
+local function LogError(errno, err, ...)
     if not errno then errno = "na" end
     if not err then err = "na" end
-    local str = os.date("%Y-%m-%d_%H%M%S") .. " - Error:" .. errno .. "- " .. err .. "\n"
-    file.write(i.debug_file_path, str, 'a')
-    if debugOut then
-        print(str)
-    end
+    Log("Error", "%s", errno, err, ...)
+
 end
 
 --- Writes a line to the log. Appends Linefeed.
 -- param: message - string for logging
-local function WriteLog(message)
-
-    local str = os.date("%Y-%m-%d_%H%M%S") .. message .. "\n"
-    file.write(i.debug_file_path, str, 'a')
-    if DEBUG then
-        print(str)
-    end
+local function LogInfo(message)
+    Log("Info", "%s", message)
 end
+
 
 
 --- Get a UUID from the OS
@@ -89,12 +97,12 @@ end
 --- InitReceive. Starts the CQ wrap that listens on the websocket
 -- param: cq - The cqueue to which we will add the routine
 -- param: ws - The websocket reference
-local function InitReceive(cq, ws)
+local function Receive(cq, ws)
     cq:wrap(function()
         repeat
             local response, err, errno = ws:receive() -- does this return an error message if it fails?
             if not response then
-                WriteError(err, errno, DEBUG)
+                LogError(err, errno)
                 ws:close()
                 Shutdown = true
             else
@@ -109,115 +117,112 @@ end
 -- param: cq - The cqueue to which we will add the routine
 -- param: ws - The websocket reference
 -- param: sleepPeriod - The periodicity of the status update
-local function InitStatusUpdate(cq, ws, sleepPeriod)
-    cq:wrap(function()
-        repeat
-            local msg = message1.new()
-            msg.uuid = GetUUID()
-            local items = i.ReadInstrumentation()
-            for k, v in pairs(items) do
-                msg.body[k] = v
-            end
+local function StatusUpdate(ws, sleepPeriod)
+    repeat
+        local msg = message1.new()
+        msg.uuid = GetUUID()
+        local items = i.ReadInstrumentation()
+        for k, v in pairs(items) do
+            msg.body[k] = v
+        end
 
-            str = json.encode(msg)
-            local success, err, errno = ws:send(str)
-            if not success then
-                WriteError(err, errno, DEBUG)
-            end
-            --This value should come from the config file.
-            cqueues.sleep(sleepPeriod)
-        until Shutdown == true
-    end)
+        str = json.encode(msg)
+        local success, err, errno = ws:send(str)
+        if not success then
+            LogError(err, errno)
+        end
+        --This value should come from the config file.
+        cqueues.sleep(sleepPeriod)
+    until Shutdown == true
 end
 
 --- InitStdioInput. A cq wrap for input from stdio. It's
 -- purpose is for manual inupt and debugging.
 -- param: cq - The cqueue to which we will add the routine
-local function InitStdioInput(cq)
-        cq:wrap(function()
-            repeat
-                io.stdout:write("Input> ")
-                cqueues.poll({ pollfd = 0; events = "r" }) -- wait until data ready to read on stdin
-                local data = io.stdin:read "*l" -- blockingly read a line. shouldn't block if tty is in line buffered mode.
-                --                assert(ws:send(data)) -- echo it back?
-            until Shutdown == true
-        end)
+local function StdioInput()
+
+    repeat
+        io.stdout:write("Input> ")
+        cqueues.poll({ pollfd = 0; events = "r" }) -- wait until data ready to read on stdin
+        local data = io.stdin:read "*l" -- blockingly read a line. shouldn't block if tty is in line buffered mode.
+        print(data)
+        if data:upper() == "SHUTDOWN" then StopServices() end;
+    until Shutdown == true
 end
 
 --- InitDebugInput. Creates sample data for testing.
 -- param: cq - The cqueue to which we will add the routine
-local function InitDebugInput(cq)
-    cq:wrap(function()
-        repeat
-            local bt = "board_temperature"
-            local nv = "new_value_2"
+local function DebugInput()
+    repeat
+        local bt = "board_temperature"
+        local nv = "new_value_2"
 
-            local bt_val
-            local nv_val
+        local bt_val
+        local nv_val
 
-            bt_val = 152
-            nv_val = 999
+        bt_val = 152
+        nv_val = 999
 
-            if i[bt] ~= nil then
-                bt_val = i[bt] + 152
-            end
-            upd(bt, bt_val)
+        if i[bt] ~= nil then
+            bt_val = i[bt] + 152
+        end
+        upd(bt, bt_val)
 
-            if i[nv] ~= nil then
-                nv_val = i[nv] + 3
-            end
+        if i[nv] ~= nil then
+            nv_val = i[nv] + 3
+        end
 
-            upd(nv, nv_val)
+        upd(nv, nv_val)
 
-            if DEBUG then
-                print(nv, nv_val)
-                print(bt, bt_val)
-            end
-            cqueues.sleep(10)
-        until Shutdown == true
-    end)
+        if DEBUG then
+            print(nv, nv_val)
+            print(bt, bt_val)
+            print(Shutdown)
+        end
+        cqueues.sleep(10)
+    until Shutdown == true
 end
 
 --- StartWraps. Start all the wraps. This give us ~some logging
 -- if the step fails.
 -- param:cq - The cqueue to start the routines on.
-local function StartWraps(cq)
-    local success, message
-    repeat
-        success, message = cq:step()
-    until not success or cq:empty()
-    return success, message
+local function Initialize(cq, ws)
+
+    assert(cq:wrap(Receive, ws))
+    assert(cq:wrap(StdioInput))
+    assert(cq:wrap(DebugInput))
+    assert(cq:wrap(StatusUpdate, ws, conf.status_period))
+
+    return true
 end
 
 local function Begin()
-    WriteError(0, "Begin")
+    debug_file = io.open(i.debug_file_path, 'a')
+
+    LogInfo("Starting client service on " .. os.date("%b %d, %Y %X"))
+
     local cq = cqueues.new()
     local ws = websocket.new_from_uri("ws://" .. conf.server_url .. ":" .. conf.server_port)
     assert(ws:connect())
 
-    InitDebugInput(cq, ws)
-    InitStatusUpdate(cq, ws, conf.status_period)
-    InitReceive(cq, ws)
-    local success, err
-    repeat
-        success, err = StartWraps(cq)
-        if not success then
-            WriteLog("The main cqueue failed to step.")
-            WriteError(0, err, DEBUG)
-        end
-    until Shutdown == true
+    local init_ok = Initialize(cq, ws)
+
+    if init_ok then
+        repeat
+            local cq_ok, msg, errno = cq:loop()
+            if not cq_ok then
+                LogInfo("The main cqueue failed to step.")
+                LogError(errno, msg)
+            end
+        until Shutdown == true or cq:empty()
+    else
+        LogError(-1, "Failed to initialize the sub routines for the application.")
+    end
 end
 
-
-local function Shutdown()
-    Shutdown = true
-end
 
 
 Begin()
-
-
-
 
 --local http_request = require "http.request"
 --local headers, stream = assert(http_request.new_from_uri("http://example.com"):go())
