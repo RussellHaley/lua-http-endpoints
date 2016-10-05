bf59a6b1e3bf3eb7adf4f7841e4fd54566add419
russellh <rhaley@gmail.com>
2016-10-04 22:31:24 -0700

    Code Cleanup.

--- exb_client.lua
+++ exb_client.lua
@@ -39,14 +39,9 @@ local Shutdown = false
 --- Debug flag. Enables Debugging output from the client.
 DEBUG = arg[1] or false
 
-local function StopServices()
-    Shutdown = true
-    print("shutdown", Shutdown)
-end
-
 local function Log(level, fmt, ...)
     local pkt = os.date("%Y-%m-%d_%H%M%S") .. "-" .. level .. ": " .. string.format(fmt, ...) .. "\n";
-    if level == "error" then
+    if level:upper() == "ERROR" then
         pkt = debug.traceback(pkt, 2)
     end;
     if DEBUG then
@@ -64,8 +59,7 @@ end
 local function LogError(errno, err, ...)
     if not errno then errno = "na" end
     if not err then err = "na" end
-    Log("Error", "%s", errno, err, ...)
-
+    Log("ERROR", "%s", errno, err, ...)
 end
 
 --- Writes a line to the log. Appends Linefeed.
@@ -97,19 +91,17 @@ end
 --- InitReceive. Starts the CQ wrap that listens on the websocket
 -- param: cq - The cqueue to which we will add the routine
 -- param: ws - The websocket reference
-local function Receive(cq, ws)
-    cq:wrap(function()
-        repeat
-            local response, err, errno = ws:receive() -- does this return an error message if it fails?
-            if not response then
-                LogError(err, errno)
-                ws:close()
-                Shutdown = true
-            else
-                print(response)
-            end
-        until not response or response:upper() == "QUIT" or Shutdown == true
-    end)
+local function Receive(ws)
+    repeat
+        local response, err, errno = ws:receive() -- does this return an error message if it fails?
+        if not response then
+            LogError(err, errno)
+            ws:close()
+            Shutdown = true
+        else
+            print(response)
+        end
+    until not response or Shutdown == true
 end
 
 --- InitStatusUpdate. Starts the cqueue wrap for sending
@@ -136,19 +128,7 @@ local function StatusUpdate(ws, sleepPeriod)
     until Shutdown == true
 end
 
---- InitStdioInput. A cq wrap for input from stdio. It's
--- purpose is for manual inupt and debugging.
--- param: cq - The cqueue to which we will add the routine
-local function StdioInput()
 
-    repeat
-        io.stdout:write("Input> ")
-        cqueues.poll({ pollfd = 0; events = "r" }) -- wait until data ready to read on stdin
-        local data = io.stdin:read "*l" -- blockingly read a line. shouldn't block if tty is in line buffered mode.
-        print(data)
-        if data:upper() == "SHUTDOWN" then StopServices() end;
-    until Shutdown == true
-end
 
 --- InitDebugInput. Creates sample data for testing.
 -- param: cq - The cqueue to which we will add the routine
@@ -159,6 +139,9 @@ local function DebugInput()
 
         local bt_val
         local nv_val
+
+        --        local i
+        --        i = 6
 
         bt_val = 152
         nv_val = 999
@@ -183,19 +166,38 @@ local function DebugInput()
     until Shutdown == true
 end
 
+local function StopServices()
+    Shutdown = true
+
+    LogInfo("System shutdown initiated.")
+end
+
+
+--- InitStdioInput. A cq wrap for input from stdio. It's
+-- purpose is for manual inupt and debugging.
+-- param: cq - The cqueue to which we will add the routine
+local function StdioInput()
+    repeat
+        io.stdout:write("Input> ")
+        cqueues.poll({ pollfd = 0; events = "r" }) -- wait until data ready to read on stdin
+        local data = io.stdin:read "*l" -- blockingly read a line. shouldn't block if tty is in line buffered mode.
+        print(data)
+        if data:upper() == "SHUTDOWN" then StopServices() end;
+    until Shutdown == true
+end
+
 --- StartWraps. Start all the wraps. This give us ~some logging
 -- if the step fails.
 -- param:cq - The cqueue to start the routines on.
 local function Initialize(cq, ws)
-
-    assert(cq:wrap(Receive, ws))
-    assert(cq:wrap(StdioInput))
-    assert(cq:wrap(DebugInput))
-    assert(cq:wrap(StatusUpdate, ws, conf.status_period))
-
+    cq:wrap(Receive, ws)
+    cq:wrap(StdioInput)
+    cq:wrap(DebugInput)
+    cq:wrap(StatusUpdate, ws, conf.status_period)
     return true
 end
 
+--- Starts all processing in the applicaiton.
 local function Begin()
     debug_file = io.open(i.debug_file_path, 'a')
 
@@ -203,20 +205,26 @@ local function Begin()
 
     local cq = cqueues.new()
     local ws = websocket.new_from_uri("ws://" .. conf.server_url .. ":" .. conf.server_port)
-    assert(ws:connect())
+    local ws_ok, err, errno = ws:connect()
+    if ws_ok then
+        local init_ok = Initialize(cq, ws)
 
-    local init_ok = Initialize(cq, ws)
+        if init_ok then
+            repeat
+                local cq_ok, msg, errno = cq:step()
+                if cq_ok then
+                    LogInfo("Step")
+                else
 
-    if init_ok then
-        repeat
-            local cq_ok, msg, errno = cq:loop()
-            if not cq_ok then
-                LogInfo("The main cqueue failed to step.")
-                LogError(errno, msg)
-            end
-        until Shutdown == true or cq:empty()
+                    LogInfo("The main cqueue failed to step.")
+                    LogError(errno, msg)
+                end
+            until Shutdown == true or cq:empty()
+            ws:close()
+        else
+            LogError(99, "Failed to initialize the sub routines for the application.")
+        end
     else
-        LogError(-1, "Failed to initialize the sub routines for the application.")
     end
 end
 
--- exb_server.lua
+++ exb_server.lua
@@ -3,8 +3,6 @@
 -- @copyright 2016
 -- @license BSD 2 Clause. See License.txt
 
-local port = arg[1] or "8000"
-
 local nice_server = require "nice_server"
 local websocket = require "http.websocket"
 local dkjson = require "dkjson"
@@ -33,45 +31,64 @@ local function reply(resp)
     for k, v in pairs(resp.request_headers) do
         print(k, v)
     end
-    print(resp.request_headers:get ":uri")
-    local ws = websocket.new_from_stream(resp.stream, resp.request_headers)
-    assert(ws:accept())
-    assert(ws:send("Welcome To LuaSocketServer"))
 
-    --    local cq = cqueues
+    local scheme = resp.request_headers:get(":scheme")
+    local upgrade
+    if resp.request_headers:get("upgrade") then
+        upgrade = resp.request_headers:get("upgrade")
+    else
+        upgrade = "none"
+    end
+    local sws_version
 
-    --[[
-    --Check if the input can be parsed.
-     - -
-     ]]
-    repeat
-        local data = assert(ws:receive())
-        if data ~= nil then
-            local json, pos, err = dkjson.decode(data, 1, nil)
-            if not err then
-                print(serpent.dump(json))
+    if resp.request_headers:get("sec-websocket-version") then
+        sws_version = tonumber(resp.request_headers:get("sec-websocket-version"))
+    else
+        sws_version = 0
+    end
 
-                if json[1] == "ECHO" then
-                    print "echo echo echoecho..."
-                    ws:send("echo2")
-                elseif json[1] == "TO BAD SAM" then
-                    print "Nice knownin ya bud"
-                    ws:send("echo2")
-                elseif json[1] == "ECHO3" then
-                    print "now this is just silly"
-                    ws:send("echo2")
+
+
+    if scheme:upper() == "HTTP" then
+
+        if upgrade:upper() == "WEBSOCKET" and sws_version >= 13 then
+            local ws = websocket.new_from_stream(resp.stream, resp.request_headers)
+            assert(ws:accept())
+            assert(ws:send("Welcome To exb Server"))
+
+            repeat
+                local data = assert(ws:receive())
+                if data ~= nil then
+                    local json, pos, err = dkjson.decode(data, 1, nil)
+                    if not err then
+                        print(serpent.dump(json))
+
+                        if json[1] == "ECHO" then
+                            print "echo echo echoecho..."
+                            ws:send("echo2")
+                        elseif json[1] == "TO BAD SAM" then
+                            print "Nice knownin ya bud"
+                            ws:send("echo2")
+                        elseif json[1] == "ECHO3" then
+                            print "now this is just silly"
+                            ws:send("echo2")
+                        end
+                    else
+                        print(data)
+                        print(err)
+                        ws:send(data)
+                    end
+                else
+                    print "NIL DATA"
                 end
-            else
-                print(data)
-                print(err)
-                ws:send(data)
-            end
+
+            until not data or data == "QUIT"
         else
-            print "NIL DATA"
+            resp.body = "No can do bub"
         end
-
-    until not data or data == "QUIT"
-
+    else
+        resp.body = "Not an http request"
+    end
 
     --Not used right now.
     --local req_body = assert(resp.stream:get_body_as_string(timeout))
